41144e10e18e94412b1fbab288260c59703bfb9e
shiyan <shiyanshowkat93@gmail.com>
2026-04-18 19:44:50 +0530

    changdd

--- src/App.jsx
+++ src/App.jsx
@@ -7,49 +7,11 @@ import Todos from "./todo.jsx";
 
 const API = "https://todo-app-backend-gfh3.onrender.com";
 
-/* 🔥 HOME REDIRECT (AUTH CHECK) */
-function HomeRedirect() {
-  const [loading, setLoading] = useState(true);
-  const [auth, setAuth] = useState(false);
-
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        const res = await fetch(`${API}/api/v1/me`, {
-          credentials: "include",
-        });
-
-        setAuth(res.ok);
-      } catch (err) {
-        setAuth(false);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    checkAuth();
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="h-screen flex items-center justify-center text-white">
-        Loading...
-      </div>
-    );
-  }
-
-  return auth ? (
-    <Navigate to="/todos" replace />
-  ) : (
-    <Navigate to="/signup" replace />
-  );
-}
-
 /* 🔥 MAIN APP */
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomeRedirect />} />
+      {/* <Route path="/" element={<HomeRedirect />} /> */}
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route path="/todos" element={<Todos />} />
